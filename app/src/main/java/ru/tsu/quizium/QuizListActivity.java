59f282fd7c60515be6cf54b550376f845cdfce57
Danil Berdnikov <danil.b0101@bk.ru>
2021-04-12 13:50:36 +0700

    Set RecyclerViewAdapter as adapter for quizRecyclerView and add test data

--- app/src/main/java/ru/tsu/quizium/QuizListActivity.java
+++ app/src/main/java/ru/tsu/quizium/QuizListActivity.java
@@ -1,14 +1,32 @@
 package ru.tsu.quizium;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizListActivity extends AppCompatActivity {
+
+    private final ArrayList<Quiz> quizList = new ArrayList<Quiz>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_list);
+
+        InitQuizData();
+        RecyclerView recyclerView = findViewById(R.id.quizRecyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, quizList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void InitQuizData() {
+        quizList.add(new Quiz("Quiz 1", "Description of Quiz 1", "admin", getDrawable(R.drawable.english_grammar)));
+        quizList.add(new Quiz("Quiz 2", "Description of Quiz 2", "admin", getDrawable(R.drawable.english_vocabulary)));
+        quizList.add(new Quiz("Quiz 3", "Description of Quiz 3", "admin", getDrawable(R.drawable.math)));
+        quizList.add(new Quiz("Quiz 4", "Description of Quiz 4", "admin", getDrawable(R.drawable.math)));
     }
 }
